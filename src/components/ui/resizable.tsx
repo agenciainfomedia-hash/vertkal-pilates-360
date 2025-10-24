c18f93fd773167bed2e8f18713c960ebdd2dfd09
[dyad] <agenciainfomedia@gmail.com>
2025-10-23 21:55:40 -0300

    [dyad] Fixed TypeScript errors and warnings. - wrote 5 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -9,18 +9,19 @@ import { cn } from "@/lib/utils"
 const ResizablePanelGroupComponent = PanelGroup
 const ResizablePanelComponent = Panel
 
-// Updated to a standard function component for React 19 ref handling
-const ResizableHandleComponent = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & { withHandle?: boolean }) => (
+const ResizableHandleComponent = React.forwardRef<
+  React.ElementRef<typeof PanelResizeHandle>,
+  React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
+    withHandle?: boolean
+  }
+>(({ withHandle, className, ...props }, ref) => (
   <PanelResizeHandle
+    ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
     )}
-    {...props} // The ref is passed through here
+    {...props}
   >
     {withHandle && (
       <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
@@ -28,7 +29,7 @@ const ResizableHandleComponent = ({
       </div>
     )}
   </PanelResizeHandle>
-)
+))
 ResizableHandleComponent.displayName = "ResizableHandle"
 
 export {
