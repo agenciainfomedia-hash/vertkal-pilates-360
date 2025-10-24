07433a0ed01298766a5cba8d6d75a8a0d778e30f
[dyad] <agenciainfomedia@gmail.com>
2025-10-23 22:47:19 -0300

    [dyad] Replaced the login screen logo with the provided image, ensuring it displays correctly and responsively. - wrote 1 file(s)

--- src/components/ui/theme-provider.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps as NextThemesProviderProps } from "next-themes"
-
-export function ThemeProvider({ children, ...props }: NextThemesProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
--- src/types/react-resizable-panels.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { PanelResizeHandleProps as OriginalPanelResizeHandleProps } from 'react-resizable-panels';
-
-declare module 'react-resizable-panels' {
-  // Augment the props interface for PanelResizeHandle
-  // This tells TypeScript that PanelResizeHandleProps can also have a 'ref'
-  interface PanelResizeHandleProps extends OriginalPanelResizeHandleProps, React.RefAttributes<HTMLDivElement> {}
-}
