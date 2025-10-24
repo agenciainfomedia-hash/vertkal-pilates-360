7b60deac7049f333acc235223d9f126685e0d349
[dyad] <agenciainfomedia@gmail.com>
2025-10-23 22:00:39 -0300

    [dyad] Fixed TypeScript ref error. - wrote 2 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -9,14 +9,14 @@ import { cn } from "@/lib/utils"
 const ResizablePanelGroupComponent = PanelGroup
 const ResizablePanelComponent = Panel
 
-const ResizableHandleComponent = React.forwardRef<
-  React.ElementRef<typeof PanelResizeHandle>,
-  React.ComponentPropsWithoutRef<typeof PanelResizeHandle> & {
-    withHandle?: boolean
-  }
->(({ withHandle, className, ...props }, ref) => (
+const ResizableHandleComponent = ({
+  withHandle,
+  className,
+  ...props
+}: React.ComponentPropsWithRef<typeof PanelResizeHandle> & {
+  withHandle?: boolean
+}) => (
   <PanelResizeHandle
-    ref={ref}
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
@@ -29,7 +29,7 @@ const ResizableHandleComponent = React.forwardRef<
       </div>
     )}
   </PanelResizeHandle>
-))
+)
 ResizableHandleComponent.displayName = "ResizableHandle"
 
 export {
