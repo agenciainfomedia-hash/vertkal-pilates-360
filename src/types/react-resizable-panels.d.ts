4712f2f3cb02ed6b111b688a2f9cfee0d06fde9e
[dyad] <agenciainfomedia@gmail.com>
2025-10-23 22:31:07 -0300

    [dyad] Suggested rebuilding the app to fix a missing node_modules file error. - wrote 4 file(s)

--- src/types/react-resizable-panels.d.ts
+++ src/types/react-resizable-panels.d.ts
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { PanelResizeHandleProps as OriginalPanelResizeHandleProps } from 'react-resizable-panels';
 
 declare module 'react-resizable-panels' {
   // Augment the props interface for PanelResizeHandle
   // This tells TypeScript that PanelResizeHandleProps can also have a 'ref'
-  interface PanelResizeHandleProps extends React.RefAttributes<HTMLDivElement> {}
+  interface PanelResizeHandleProps extends OriginalPanelResizeHandleProps, React.RefAttributes<HTMLDivElement> {}
 }
