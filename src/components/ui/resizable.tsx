2fe344881daf65f4caeebc5d70df4a7cb72bfe91
[dyad] <agenciainfomedia@gmail.com>
2025-10-23 22:25:46 -0300

    [dyad] Fixed TypeScript ref error by re-implementing type augmentation and simplifying component props. - wrote 4 file(s), deleted 1 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -9,17 +9,18 @@ import { cn } from "@/lib/utils"
 const ResizablePanelGroupComponent = PanelGroup
 const ResizablePanelComponent = Panel
 
-// Define the props for our ResizableHandleComponent, now relying on the augmented PanelResizeHandleProps
-interface ResizableHandleComponentProps extends PanelResizeHandleProps {
+// Combine PanelResizeHandleProps with standard HTMLDivElement attributes
+// to ensure className and ref are present, and add our custom 'withHandle' prop.
+interface ResizableHandleComponentProps extends PanelResizeHandleProps, React.HTMLAttributes<HTMLDivElement> {
   withHandle?: boolean;
 }
 
 const ResizableHandleComponent = React.forwardRef<
   HTMLDivElement, // The type of the DOM element the ref will attach to
-  ResizableHandleComponentProps // Use our custom props interface, which now correctly includes 'ref'
+  ResizableHandleComponentProps // Use our custom props interface
 >(({ withHandle, className, ...props }, ref) => (
   <PanelResizeHandle
-    ref={ref} // This should now be correctly typed due to the augmentation
+    ref={ref} // This should now be correctly typed
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
