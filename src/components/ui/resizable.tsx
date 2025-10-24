df899e01c07437ef90c31832f92353cacc443cc3
[dyad] <agenciainfomedia@gmail.com>
2025-10-23 22:22:58 -0300

    [dyad] Fixed TypeScript ref error by re-implementing type augmentation and simplifying component props. - wrote 5 file(s), deleted 1 file(s)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -2,21 +2,24 @@
 
 import * as React from "react"
 import { GripVerticalIcon } from "lucide-react"
-import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels"
+import { PanelGroup, Panel, PanelResizeHandle, PanelResizeHandleProps } from "react-resizable-panels"
 
 import { cn } from "@/lib/utils"
 
 const ResizablePanelGroupComponent = PanelGroup
 const ResizablePanelComponent = Panel
 
+// Define the props for our ResizableHandleComponent, now relying on the augmented PanelResizeHandleProps
+interface ResizableHandleComponentProps extends PanelResizeHandleProps {
+  withHandle?: boolean;
+}
+
 const ResizableHandleComponent = React.forwardRef<
   HTMLDivElement, // The type of the DOM element the ref will attach to
-  React.ComponentPropsWithRef<typeof PanelResizeHandle> & { // This type includes ref and className
-    withHandle?: boolean
-  }
+  ResizableHandleComponentProps // Use our custom props interface, which now correctly includes 'ref'
 >(({ withHandle, className, ...props }, ref) => (
   <PanelResizeHandle
-    ref={ref} // This should now be correctly typed
+    ref={ref} // This should now be correctly typed due to the augmentation
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90",
       className
